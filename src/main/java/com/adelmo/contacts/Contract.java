98078c12ca72579d7c7800aa26ee6d0422019c87
jacknb <zhuningbin@sina.com>
2017-07-24 23:43:38 +0800

    contact

--- src/main/java/com/adelmo/contacts/Contract.java
+++ src/main/java/com/adelmo/contacts/Contract.java
@@ -4,4 +4,50 @@ package com.adelmo.contacts;
  * Created by znb on 17-7-24.
  */
 public class Contract {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneName;
+    private String emailAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneName() {
+        return phoneName;
+    }
+
+    public void setPhoneName(String phoneName) {
+        this.phoneName = phoneName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
--- src/main/java/com/adelmo/contacts/ContractRepository.java
+++ src/main/java/com/adelmo/contacts/ContractRepository.java
@@ -1,14 +1,41 @@
 package com.adelmo.contacts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by znb on 17-7-24.
  */
+
+@Repository
 public class ContractRepository {
 
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ContractRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public List<Contract> findAll() {
-        return null;
+        String sql = "select id,firstName,lastName,phoneName,emailAddress from contacts order by lastName";
+        return jdbcTemplate.query(sql, new RowMapper<Contract>() {
+            @Override
+            public Contract mapRow(ResultSet resultSet, int i) throws SQLException {
+                Contract contract = new Contract();
+                contract.setId(resultSet.getLong(1));
+                contract.setFirstName(resultSet.getString(2));
+                contract.setLastName(resultSet.getString(3));
+                contract.setPhoneName(resultSet.getString(4));
+                contract.setEmailAddress(resultSet.getString(5));
+                return contract;
+            }
+        });
     }
 }
